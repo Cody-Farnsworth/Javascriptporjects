2857942185940a95e6de9a43fae41773355abedb
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2021-08-04 18:44:17 -0600

    Update main.js

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,13 +1,11 @@
-function My_Second_Function() {
+function My_Second_Function() {                                   // This function makes the text "This is green", and changes the color to blue
     var str = "This text is green!";
-
     var result = str.fontcolor("Blue");
-
-    document.getElementById("Green_Text").innerHTML=result;
+    document.getElementById("Green_Text").innerHTML=result;       //Putting the value of resultinto HTML element 
 }
 
-function myFunction() {
+function myFunction() {                                          //This function concatenates the var sentence togeather
     var sentence = "Oh no that didnt come out right";
-    sentence += "it should have been green!";
-    document.getElementById("Concatenate").innerHTML=sentence;
+    sentence += " it should have been green!";
+    document.getElementById("Concatenate").innerHTML=sentence      //Putting the value of resultinto HTML element 
 }
